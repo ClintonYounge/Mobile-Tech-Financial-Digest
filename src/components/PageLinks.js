8f12a6c5a2c2510b644497f38eba433c5de8ad25
ClintonYounge <clintonyounge18@gmail.com>
2023-06-26 14:33:29 -0400

    update pagelinks code

--- src/components/PageLinks.js
+++ src/components/PageLinks.js
@@ -4,20 +4,24 @@ const PageLinks = () => (
   <>
     <nav className="page-links">
       <ul className="page-link">
-        <li><Link to="/apple">Apple</Link></li>
-        <li>*** Billion for the year ***</li>
+        <li>Apple</li>
+        <li>Apple netted $99,803,000,000 USD for the year 2022</li>
+        <li><Link to="/apple">See more financial data</Link></li>
       </ul>
       <ul className="page-link">
-        <li><Link to="/samsung">Samsung</Link></li>
-        <li>*** Billion for the year ***</li>
+        <li>Samsung</li>
+        <li>Samsung netted ₩54,730,018,000,000 KRW for the year 2022</li>
+        <li><Link to="/samsung">See more financial data</Link></li>
       </ul>
       <ul className="page-link">
-        <li><Link to="/xiaomi">Xiaomi</Link></li>
-        <li>*** Billion for the year ***</li>
+        <li>Xiaomi</li>
+        <li>Xiaomi netted ¥2,474,030,000 CNY for the year 2022</li>
+        <li><Link to="/xiaomi">See more financial data</Link></li>
       </ul>
       <ul className="page-link">
-        <li><Link to="/google">Google</Link></li>
-        <li>*** Billion for the year ***</li>
+        <li>Google</li>
+        <li>Google netted $59,972,000,000 USD for the year 2022</li>
+        <li><Link to="/google">See more financial data</Link></li>
       </ul>
     </nav>
   </>
